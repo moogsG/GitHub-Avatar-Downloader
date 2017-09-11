b1b493b200d550a65d60f014a9818b6c3f4fadba
Morgan Greff <morgan@greffs.com>
2017-09-11 17:38:00 +0000

    Makes request, and gets json feed

--- downloaded_avatars.js
+++ downloaded_avatars.js
@@ -1,3 +1,31 @@
 var request = require('request');
+var fs = require('fs');
+var GITHUB_USER = "moogsG";
+var GITHUB_TOKEN = "ee33c2e1000e6db8b1c68756fa90a9c7dbb0c374";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'moogsG'
+    }
+  };
+  console.log(requestURL);
+  request.get(options)
+    .on('error', function(err){
+      throw err;
+    })
+    .on('response', function(response){
+      console.log('Response Status Code: ', response.statusCode);
+      console.log('Response Message: ', response.statusMessage);
+      console.log('Response Content Type: ', response.headers['content-type']);
+    })
+  .pipe(fs.createWriteStream('./tmp/tmp.json'));
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
